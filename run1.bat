5677b8929cdb1166d1a3dd030a59801c00fb61cd
sri052010 <47378173+sri052010@users.noreply.github.com>
2020-07-18 20:58:12 +0530

    Add files via upload
    latest file

--- run1.bat
+++ run1.bat
@@ -1,1 +1,6 @@
-Java –cp bin;lib/* org.testng.TestNG testng.xml
+set projectLocation=C:\Srikanth\CucumberProject\Githubjenktest1
+cd %projectLocation%
+set classpath=C:\Srikanth\CucumberProject\Githubjenktest1\target\classes\;C:\Srikanth\CucumberProject\Githubjenktest1\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
+
